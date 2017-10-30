cc84d80d6ab361738165e7c0b3774ca251330f9d
Tom Smith <tom@fizzynova.com>
2017-10-30 09:25:06 +0000

    Moved portfolio logos into portfolio subdir

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,14 @@ var gulp = require('gulp');
 var changed = require("gulp-changed");
 var imageResize = require('gulp-image-resize');
 
-gulp.task('resize', function () {
-  gulp.src('./public_src/images/*.{jpg,png}')
-    .pipe(changed('./public/images/'))
+gulp.task('resize_portfolio_images', function () {
+  gulp.src('./public_src/images/portfolio/*.{jpg,png}')
+    .pipe(changed('./public/images/portfolio/'))
     .pipe(imageResize({
       width : 200,
       height : 200,
       crop : false,
       upscale : false
     }))
-    .pipe(gulp.dest('./public/images/'));
+    .pipe(gulp.dest('./public/images/portfolio/'));
 });
