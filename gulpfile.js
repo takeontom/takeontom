8b96f54b92e78777f8b7bbb68e91cc28a1e878f5
Tom Smith <tom@fizzynova.com>
2017-10-30 10:36:49 +0000

    Full gulp build. public dir now transient

--- gulpfile.js
+++ gulpfile.js
@@ -4,10 +4,22 @@ var imageResize = require('gulp-image-resize');
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
 var webpack = require('webpack-stream');
+var del = require('del');
+var gulpSequence = require('gulp-sequence')
 
-gulp.task('watch', ['resize_portfolio_images:watch', 'sass:watch', 'js:watch'], () => {
+gulp.task('watch', ['resize_portfolio_images:watch', 'sass:watch', 'js:watch', 'fonts:watch'], () => {
   console.log('Watchers started');
 })
+
+gulp.task('build', ['resize_portfolio_images', 'sass', 'js', 'fonts'], () => {
+  console.log('Build complete');
+})
+
+gulp.task('build:full', gulpSequence('clean:public', 'build'));
+
+gulp.task('clean:public', () => {
+  return del(['./public/**/*']);
+});
 
 gulp.task('sass', () => {
   return gulp.src('./public_src/stylesheets/takeontom.scss')
@@ -15,6 +27,10 @@ gulp.task('sass', () => {
   .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
   .pipe(sourcemaps.write('./maps'))
   .pipe(gulp.dest('./public/css/'));
+});
+
+gulp.task('sass:watch', ['sass'], () => {
+  gulp.watch('./public_src/stylesheets/**/*.*', ['sass']);
 });
 
 gulp.task('js', () => {
@@ -29,11 +45,16 @@ gulp.task('js:watch', ['js'], () => {
   gulp.watch('./public_src/js/**/*.*', ['js']);
 });
 
-gulp.task('sass:watch', ['sass'], function () {
-  gulp.watch('./public_src/stylesheets/**/*.*', ['sass']);
-});
+gulp.task('fonts', () => {
+  return gulp.src('./public_src/font/*.*')
+  .pipe(gulp.dest('./public/font/'));
+})
 
-gulp.task('resize_portfolio_images', function () {
+gulp.task('fonts:watch', ['fonts'], () => {
+  gulp.watch('./public_src/font/**/*.*', ['fonts']);
+})
+
+gulp.task('resize_portfolio_images', () => {
   return gulp.src('./public_src/images/portfolio/*.{jpg,png}')
     .pipe(changed('./public/images/portfolio/'))
     .pipe(imageResize({
@@ -45,6 +66,6 @@ gulp.task('resize_portfolio_images', function () {
     .pipe(gulp.dest('./public/images/portfolio/'));
 });
 
-gulp.task('resize_portfolio_images:watch', ['resize_portfolio_images'], function () {
+gulp.task('resize_portfolio_images:watch', ['resize_portfolio_images'], () => {
   gulp.watch('./public_src/images/portfolio/*.{jpg,png}', ['resize_portfolio_images']);
 });
